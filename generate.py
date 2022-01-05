105f8b3411e0a259ddfd50ecd79919c8a3f9f530
Connor Rhodes <connor@rhodes.contact>
2022-01-04 21:00:09 -0600

    added explanatory comments

--- generate.py
+++ generate.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python
+# Made using this tutorial: https://medium.com/@jasonrigden/jinja2-templating-engine-tutorial-4bd31fb4aea3
+
+#import jinja2
 from jinja2 import Environment, FileSystemLoader
 
+# pass the directory containing the templates to FileSystemLoader
 file_loader = FileSystemLoader('templates')
 
+# load the environment
 env = Environment(loader=file_loader)
 
+# load the template
 template = env.get_template('hello_world.txt')
 
+# render and pring
 output = template.render()
 print(output)
