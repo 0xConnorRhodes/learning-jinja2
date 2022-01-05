7622db0013f9f17ff8cc8ddc5f323edf75f02d95
Connor Rhodes <connor@rhodes.contact>
2022-01-04 20:58:44 -0600

    added code to print the template file

--- generate.py
+++ generate.py
@@ -4,3 +4,8 @@ from jinja2 import Environment, FileSystemLoader
 file_loader = FileSystemLoader('templates')
 
 env = Environment(loader=file_loader)
+
+template = env.get_template('hello_world.txt')
+
+output = template.render()
+print(output)
